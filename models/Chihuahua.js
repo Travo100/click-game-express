2fd6d333458fab59e505214b81c68e7f5aa9ca8f
Travo100 <Travo100@gmail.com>
2018-02-15 10:39:46 -0800

    Stashing

---
+++ models/Chihuahua.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const chihuahuaSchema = new Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    date: { type: Date, default: Date.now }
+  });
+  
+const Chihuahua = mongoose.model("Chihuahua", chihuahuaSchema);
+  
+module.exports = Chihuahua;
